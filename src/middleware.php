38cc19015e141a484b59e5c9a86d87798a76af8f
ardian27 <ardiansyah2795@gmail.com>
2018-12-05 09:56:30 +0700

    my api apps with slim

--- src/middleware.php
+++ src/middleware.php
@@ -9,7 +9,7 @@ $app->add(function ($request, $response, $next) {
         return $response->withJson(["status" => "API Key required"], 401);
     }
     
-    $sql = "SELECT * FROM api_user_ax WHERE api_key=:api_key";
+    $sql = "SELECT * FROM api_user WHERE api_key=:api_key";
     $stmt = $this->db->prepare($sql);
     $stmt->execute([":api_key" => $key]);
     
